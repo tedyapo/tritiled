ab184b66e4621780347d23a4ee40ce6d085812c4
Ted Yapo <ted.yapo@zednaughtlabs.com>
2018-01-06 02:24:27 -0500

    added c-source for V3.0 selectable runtime

---
+++ software/tritiled30/src/HEF_access.asm
@@ -0,0 +1,36 @@
+;;;
+;;; assembly routines for High-Endurance Flash (HEF) access
+;;;
+#include <xc.inc>  
+#include <pic12lf1571.inc>
+
+;;;
+;;; read one word of flash (result in PMDAT)
+;;; 
+  GLOBAL  _read_hef
+  SIGNAT  _read_hef, 88
+  PSECT   hef_r_text,local,class=CODE,delta=2 
+_read_hef:
+    BANKSEL     PMCON1
+    bcf         CFGS
+    bsf         RD
+    nop
+    nop
+    return
+
+;;;
+;;; execute flash unlock sequence for erase or write
+;;; 
+  GLOBAL  _unlock_hef
+  SIGNAT  _unlock_hef, 88
+  PSECT   hef_w_text,local,class=CODE,delta=2
+_unlock_hef:
+    BANKSEL     PMCON2
+    movlw       0x55
+    movwf       BANKMASK(PMCON2)
+    movlw       0xaa
+    movwf       BANKMASK(PMCON2)
+    bsf         WR
+    nop
+    nop
+    return
